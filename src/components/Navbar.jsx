3066d488c784d19fab58e149d92c34ad3e768a8c
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-06-23 10:26:21 +0200

    feat: add hamburger menu icon transition

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,16 +12,31 @@ export default function Navbar() {
         <Link to={"/"} className="self-center text-2xl font-semibold whitespace-nowrap">
           S.S.
         </Link>
+
         {/* Bottone Hamburger */}
         <button
           type="button"
           onClick={() => setIsOpen(!isOpen)}
-          className="inline-flex items-center  w-10 h-10 justify-center text-sm text-black rounded md:hidden hover:bg-black hover:text-white transition duration-500"
+          className="relative w-10 h-10 flex items-center justify-center md:hidden focus:outline-none"
           aria-controls="navbar-default"
           aria-expanded={isOpen}
         >
-          {!isOpen ? <RiMenu3Fill className="w-10 h-10" /> : <IoCloseOutline className="w-10 h-10" />}
+          <div className="relative w-6 h-5">
+            <span
+              className={`absolute left-0 w-6 h-0.5 bg-black transform transition-all duration-300 ease-in-out
+             ${isOpen ? "rotate-45 translate-y-[8px]" : "translate-y-0"}`}
+            ></span>
+            <span
+              className={`absolute left-0 w-6 h-0.5 bg-black transform transition-all duration-300 ease-in-out
+            ${isOpen ? "opacity-0" : "translate-y-[8px]"}`}
+            ></span>
+            <span
+              className={`absolute left-0 w-6 h-0.5 bg-black transform transition-all duration-300 ease-in-out
+            ${isOpen ? "-rotate-45 translate-y-[8px]" : "translate-y-[16px]"}`}
+            ></span>
+          </div>
         </button>
+
         {/* Menu Navigazione */}
         <div className={`${isOpen ? "block" : "hidden"} w-full md:block md:w-auto`} id="navbar-default">
           <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:mt-0 md:border-0 md:bg-white md:space-x-2">
@@ -43,10 +58,8 @@ export default function Navbar() {
                   to={`/${item.toLowerCase()}`}
                   className={
                     "relative block py-2 px-4 text-black mb-3 md:mb-0 rounded-sm " +
-                    (isOpen ? "hover:bg-black hover:text-white transition duration-300" : "before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:bg-black before:transition-all before:duration-700 before:ease-in-out hover:before:w-full hover:text-white")
-                  }
-                  onClick={() => setIsOpen(false)}
-                >
+                    (isOpen ? "hover:bg-black hover:text-white transition duration-300" : "before:absolute before:left-0 before:top-0 before:h-full before:w-0 before:bg-black before:transition-all before:duration-700 before:ease-in-out hover:before:w-full hover:text-white")}
+                  onClick={() => setIsOpen(false)}>
                   <span className="relative z-10">{item}</span>
                 </Link>
               </li>
